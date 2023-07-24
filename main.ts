4a17d196ccc84e32a34d96efa84ff79481b2991d
Rubén Chiquin <randreu28@gmail.com>
2023-07-24 19:02:46 +0200

    v1.0.0

--- main.ts
+++ main.ts
@@ -1,5 +1,42 @@
 import { Input } from "https://deno.land/x/cliffy@v1.0.0-rc.2/prompt/mod.ts";
+import { ConversationalOutput, HfInference } from "npm:@huggingface/inference";
+import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
+import { VERSION } from "./version.ts";
+import Spinner from "https://deno.land/x/cli_spinners@v0.0.2/mod.ts";
 
-const name: string = await Input.prompt(`What's your name?`);
+await new Command()
+  .name("Llama-cli")
+  .version(VERSION)
+  .description(
+    "A CLI for interacting with DialoGPT.\n https://github.com/randreu28/llama-cli",
+  )
+  .parse(Deno.args);
 
-console.log(name);
+const hf = new HfInference();
+const spinner = Spinner.getInstance();
+
+let PREV_CONV: ConversationalOutput["conversation"] = {
+  generated_responses: [],
+  past_user_inputs: [],
+};
+
+console.log("Start chatting!");
+
+while (true) {
+  const question: string = await Input.prompt("");
+
+  spinner.start("Generating response");
+
+  const response = await hf.conversational({
+    parameters: { max_length: 500 },
+    model: "microsoft/DialoGPT-medium",
+    inputs: {
+      ...PREV_CONV,
+      text: question,
+    },
+  });
+
+  spinner.stop();
+  PREV_CONV = response.conversation;
+  console.log(response.generated_text);
+}
--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `version` managed by https://deno.land/x/land/publish. */
-export const VERSION = "0.0.2";
+export const VERSION = "1.0.0";
 
 /** `prepublish` will be invoked before publish, return `false` to prevent the publish. */
 export function prepublish(version: string) {
